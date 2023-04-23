4c3deb5f0121714ff77311112d2d49631c4f1887
galhrrera <juan.galeanoherrera@gmail.com>
2023-04-23 14:58:53 -0500

    Project structure built

---
+++ ToDo/src/main/resources/scheme.sql
@@ -0,0 +1,7 @@
+CREATE TABLE task
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    completed   BOOLEAN      NOT NULL DEFAULT FALSE
+);
