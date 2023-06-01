7a97964b4f7e70bc48f784c7a9e88dd056c09e53
galhrrera <juan.galeanoherrera@gmail.com>
2023-05-31 22:28:37 -0500

    Refactorized code - repositories and services for tasks and customers - controllers working

--- ToDo/src/main/java/com/example/todo/controller/TaskController.java
+++ ToDo/src/main/java/com/example/todo/controller/TaskController.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("api/tasks")
 public class TaskController {
-    /*private final TaskServiceInterface taskServiceInterface;*/
+
     private final TaskService taskService;
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
